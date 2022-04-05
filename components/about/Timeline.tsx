c7eb5c6909ff8ca45f91c2414446bfb6de6e4ff2
Parker Brown <brownpt98@gmail.com>
2022-04-05 00:51:15 -0500

    Better styling for timeline

--- components/about/Timeline.tsx
+++ components/about/Timeline.tsx
@@ -9,7 +9,7 @@ export default function Timeline({ items }: Props) {
 
     return (
         <ul className="flex flex-col gap-y-4 mt-8">
-            { items.map(time => <TimelineEntry key={time.title} timeline={time} />)}
+            { items.map(time => <TimelineEntry key={time.title} timeline={time} />) }
         </ul>
     )
 }
--- components/about/TimelineEntry.tsx
+++ components/about/TimelineEntry.tsx
@@ -7,11 +7,13 @@ type Props = {
 
 export default function TimelineEntry({ timeline }: Props) {
     return (
-        <li className={`relative pl-[44px] ${styles.entry}`}>
+        <li className={`relative pl-[44px] ${styles.entry} flex flex-col gap-y-1`}>
             <div className="text-3xl text-slate-600 font-bold">{ timeline.title }</div>
-            <div className="text-lg text-slate-500 font-semibold leading-tight">{ timeline.location }</div>
-            <div className="text-lg text-slate-400 font-semibold leading-tight">{ timeline.daterange }</div>
-            <div className="mt-2">{ timeline.body }</div>
+            <div>
+                <div className="text-lg text-slate-500 font-semibold leading-tight">{ timeline.location }</div>
+                <div className="text-lg text-slate-400 font-semibold leading-tight">{ timeline.daterange }</div>
+            </div>
+            <div>{ timeline.body }</div>
         </li>
     )
 }
